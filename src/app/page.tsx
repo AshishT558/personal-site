53e4c977f8a53cd5ba0af6a6f952fc9dacb9a148
AshishT558 <ashi1j6thomas@gmail.com>
2024-04-11 19:14:15 -0400

    rough first build of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import { useState } from "react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <p>
-        Hello World
-      </p>
-      <PageLink text="About Me" page="/about_me"></PageLink>
-      <Button text="hello"></Button>
+    <main className="h-screen flex flex-col items-center justify-center pb-20">
+        <h1 className="text-7xl pb-5">Hello, I'm Ashish</h1>
+        <p className="text-lg pb-20">
+          Softare Development • Data Science • Artificial Intelligence • Machine Learning
+        </p>
+        <div className="space-x-20">
+          <PageLink text="About Me" page="/about_me"></PageLink>
+          <PageLink text="Experience" page="/about_me"></PageLink>
+          <PageLink text="Projects" page="/about_me"></PageLink>
+        </div>
+      
     
     </main>
   );
