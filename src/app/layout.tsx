01fd90c094dfe91f14f6ff0ef6a47d3da23f5aee
AshishT558 <ashi1j6thomas@gmail.com>
2024-06-25 20:58:20 -0400

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
       {children}
       <Footer></Footer>
+      <Analytics />
       </body>
     </html>
   );
